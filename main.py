fd41c2fa39c1d1a1f707dc69e4a6c520d6c54250
Богдан <bodyu050505@mail.ru>
2025-10-06 18:15:14 +0200

    ADDED TASK ADDER (KOSTYLY)

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ from flask import render_template, redirect, request
 
 app = Flask(__name__)
 
+tasks = []
+
 @app.route("/")
 def index():
-    return render_template("index2.html")
+    return render_template("index2.html", tasks=tasks)
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
         user_input = request.form.get("user_input", "")
-        print(user_input)
+        if user_input:
+            tasks.append(user_input)
+        return render_template("success.html")
+    
     return render_template("add.html")
 
 @app.route("/delete")
